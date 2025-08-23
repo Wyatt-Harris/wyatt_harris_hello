7a8f7df5edb6e403ce18dd1c2c61fde909912633
Wyatt-Harris <wyeharris@gmail.com>
2025-08-23 17:22:00 -0500

    Cleaned up this folder and removed unnecessary  files, copy and pasted code from Dr. Owen

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,3 +1,17 @@
-public class Main {
-    
+public class HelloWorld { 
+    String name; 
+   //Constructor
+    public HelloWorld(String name) { 
+        this.name = name; 
+    } 
+    //Method to print greeting
+    public void greet() {
+     System.out.println("Hello, " + name + "!");
+     }
+      //Main method
+     public static void main(String[] args) {
+        HelloWorld student = new HelloWorld("Wyatt"); 
+        student.greet();
+     } 
+
 }
